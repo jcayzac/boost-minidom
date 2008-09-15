911441b24c50970e4bc5c3e1cf610d2a18b87436
julien.cayzac <julien.cayzac@e25d93a4-7ea2-11dd-827b-89176052eb68>
2008-09-15 04:28:53 +0000

    Added dom_config.hpp and fixed compilation with G++

---
+++ trunk/MiniDOM/dom_config.hpp
@@ -0,0 +1,15 @@
+#if !defined(DOM_CONFIG_INCLUDED)
+#define DOM_CONFIG_INCLUDED
+
+#if defined(__GNUC__)
+	/* GCC Wants friend classes to be specified as 'class' */
+	#define TEMPLATED_FRIEND friend class
+#elif defined(_MSC_VER)
+	/* MSVC doesn't like "friend class" at all... */
+	#define TEMPLATED_FRIEND friend
+#else
+	#define TEMPLATED_FRIEND friend class
+#endif
+
+#endif // DOM_CONFIG_INCLUDED
+
