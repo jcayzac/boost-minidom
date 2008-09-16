3f43f1e56148e077c162e134e58851dc0eee41f8
julien.cayzac <julien.cayzac@e25d93a4-7ea2-11dd-827b-89176052eb68>
2008-09-16 12:33:08 +0000

    Initial DOMReader (utf-8 only)

--- trunk/MiniDOM/dom_reader.hpp
+++ trunk/MiniDOM/dom_reader.hpp
@@ -1,9 +1,72 @@
 #if !defined(DOM_READER_INCLUDED)
 #define DOM_READER_INCLUDED
 
+#include <string>
+#include <vector>
+#include <list>
+#include "dom_types.hpp"
+
 namespace dom {
 
-
+/** A DOMReader object is used to load a DOM tree
+  * from an XML-formatted file.
+  */
+class DOMReader {
+public:
+    /** Loads a DOM tree from an XML-formatted file.
+      *
+      * There is currently no charset/encoding recognition nor translation
+      * done by the parser. XML files are assumed to be encoded in UTF-8.
+      * 
+      * Reading is done synchronously for now, so DOMReader objects are
+      * somehow reusable, but don't expect it to be true in the future when
+      * asynchronous loading comes...
+      * 
+      * @param filename XML-formatted file's name
+      * @return A shared pointer pointing at a Document object.
+      * @sa DOMException
+      */
+    DocumentPtr readDocument(const std::wstring& filename);
+private:
+    /** Parse a &lt;...&gt; block
+      * @param data Block content
+      * @internal
+      */
+    void parseAngleBracketBlock(std::wstring& data);
+    /** Parse a &lt;?...?&gt; block
+      * @param data Block content
+      * @internal
+      */
+    void parseNewHeader(std::wstring& data);
+    /** Parse a &lt;!...&gt; block
+      * @param data Block content
+      * @internal
+      */
+    void parseNewDTDElement(std::wstring& data);
+    /** Parse a &lt;.../&gt; block
+      * @param data Block content
+      * @internal
+      */
+    void parseNewTag(std::wstring& data);
+    /** Parse a &lt;...&gt; block containing no attributes
+      * @param data Block content
+      * @internal
+      */
+    void parseNewElement(std::wstring& data);
+    /** Parse attributes from &lt;...&gt; block
+      * @param data Block content after the tag name
+      * @internal
+      */
+    void parseAttributes(std::wstring& data);
+    /** Parse a &lt;/...&gt; block
+      * @param data Block content
+      * @internal
+      */
+    void closeTag(std::wstring& data);
+private:
+    std::list<NodePtr> m_oNodeStack; ///<! Parser's stack
+    DocumentPtr        m_oDocument;  ///<! Current document
+};
 
 }; // namespace dom
 
--- trunk/MiniDOM/dom_utils.hpp
+++ trunk/MiniDOM/dom_utils.hpp
@@ -1,20 +1,25 @@
 #if !defined(DOM_UTILS_INCLUDED)
 #define DOM_UTILS_INCLUDED
 
-#include <boost/lexical_cast.hpp>
-#include <vector>
-#include "dom_types.hpp"
+#include <string>
 
 namespace dom {
 
 class DOMUtils {
 public:
-    /** Translate an XML/HTML entity, such as &amp;eacute; or &amp;#67;
-      * or &amp;#xe3;, and return its character value.
+    /** Translate an XML/HTML entity, such as "&amp;eacute;" or "&amp;#67;"
+      * or "&amp;#xe3;", and return its character value.
       * @param entity Name of the entity
       * @return The corresponding character
       */
 	static wchar_t translateXMLEntity(const std::wstring& entity);
+
+    static bool isLineBreakingSpace(wchar_t c);
+    static bool isBreakingWhiteSpace(wchar_t c);
+    static bool isNonBreakingWhiteSpace(wchar_t c);
+    static bool isWhiteSpace(wchar_t c);
+    static void trimSpaces(std::wstring& s);
+
 };
 
 }; // namespace dom
